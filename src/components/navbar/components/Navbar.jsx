caf6d955160fd7a15d543c45df071fad4e101813
Edward Bella <eddydambel@gmail.com>
2019-11-27 20:40:37 +0100

    Refactored Navbar component to use the React hook useState and also moved all non-styling strings to constants,js

--- src/components/navbar/components/Navbar.jsx
+++ src/components/navbar/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, Row, Col } from "antd";
 import { menuItems } from "../constants";
@@ -8,54 +8,48 @@ import { menuItems } from "../constants";
  * It is a top level element and as such it is resident in the App.js file which is the entry system into the root index.js
  */
 
-class Navbar extends Component {
-  state = {
-    current: "home"
-  };
+const Navbar = () => {
+  const [currentNav, setCurrentNav] = useState("home");
 
   /**
-   * @function
+   * @constant
    * @listens {event} to dtermine which of the navbar element is active using the "key" parameter on the menu item
    */
-  handleClick = e => {
+  const handleClick = e => {
     console.log("click ", e);
-    this.setState({
-      current: e.key
-    });
+    setCurrentNav(e.key);
   };
 
-  render() {
-    return (
-      <nav>
-        <Row type="flex" justify="center">
-          <Col span={24}>
-            <div className="navbar" data-aos="slide-down">
-              <Menu
-                className="navbar-group"
-                onClick={this.handleClick}
-                selectedKeys={[this.state.current]}
-                mode="horizontal"
-              >
-                {menuItems.map(menu => {
-                  return (
-                    <Menu.Item key={menu.key} className={menu.className}>
-                      <Link className={menu.linkClassName} to={menu.linkTo}>
-                        {menu.img ? (
-                          <img src={menu.img} alt={menu.imgAlt} />
-                        ) : (
-                          menu.text
-                        )}
-                      </Link>
-                    </Menu.Item>
-                  );
-                })}
-              </Menu>
-            </div>
-          </Col>
-        </Row>
-      </nav>
-    );
-  }
-}
+  return (
+    <nav>
+      <Row type="flex" justify="center">
+        <Col span={24}>
+          <div className="navbar" data-aos="slide-down">
+            <Menu
+              className="navbar-group"
+              onClick={handleClick}
+              selectedKeys={[currentNav]}
+              mode="horizontal"
+            >
+              {menuItems.map(menu => {
+                return (
+                  <Menu.Item key={menu.key} className={menu.className}>
+                    <Link className={menu.linkClassName} to={menu.linkTo}>
+                      {menu.img ? (
+                        <img src={menu.img} alt={menu.imgAlt} />
+                      ) : (
+                        menu.text
+                      )}
+                    </Link>
+                  </Menu.Item>
+                );
+              })}
+            </Menu>
+          </div>
+        </Col>
+      </Row>
+    </nav>
+  );
+};
 
 export default Navbar;
