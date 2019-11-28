f5bbf0261e267b386497752729641f9576d1a1dc
Edward Bella <eddydambel@gmail.com>
2019-11-28 09:41:02 +0100

    Minor scss fix to hero-title

--- src/components/homepage/constants.js
+++ src/components/homepage/constants.js
@@ -28,7 +28,7 @@ export const HELMET_STRINGS = {
 export const MAIN_CONTENT = {
 	HERO_IMG: Artwork,
 	HERO_TITLE: "Let us help you worry-less about your laundry",
-	HERO_DESC: "CleanIT is your on-demand laundry service middle-man. As a User, we  " + "provide you the means to meet hundreds of laundry service providers with a resolve " + "to provide quality and efficient service.As a Vendor, we provide you the means to " + "meet thousands of people who require laundry services.",
+	HERO_DESC: "CleanIT is your on-demand laundry service middle-man. As a User, we  " + "provide you the means to meet hundreds of laundry service providers with a resolve " + "to provide quality and efficient service. As a Vendor, we provide you the means to " + "meet thousands of people who require laundry services.",
 	LINKS: [{
 		link_class: "btn-primary -large -mr-2",
 		link_to: "/sign-up",
