3386e40d76f4763a4573fb681d34b31f630e7e58
segun-ade <adebanjosegun589@gmail.com>
2019-11-29 19:39:34 +0100

    broke down the PR in PR #6:rocket:

--- src/components/signup/constants.js
+++ src/components/signup/constants.js
@@ -1,12 +1,13 @@
 
-import Logo from './images/logo.svg';
+const logoSuffix1 = 'cleanit-7147d.appspot.com/o/logo.svg';
+const logoSuffix2 = 'media&token=f8fed921-a485-4cb8-81e4-23069b98f801';
 
 export const STRINGS = {
     ALREADY: 'Already have an account?',
     CREATE_ACCOUNT: 'Create Account',
     JOIN: 'Join as a User',
     LOGIN: 'Login using social media to gain quick access',
-    LOGO: Logo,
+    LOGO: `https://firebasestorage.googleapis.com/v0/b/${logoSuffix1}?alt=${logoSuffix2}`,
     SIGNIN: 'Sign in',
     SIGNUP_ENDPOINT: 'https://us-central1-cleanit-7147d.cloudfunctions.net/app/api/v1/auth/',
     USER: 'user',
