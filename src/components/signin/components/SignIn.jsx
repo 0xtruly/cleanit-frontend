86f00d801e0716e5c20f0d9fa3751b14f05568d5
Edward Bella <eddydambel@gmail.com>
2019-11-30 18:38:18 +0100

    Removing ESLint manual disables

--- src/components/signin/components/SignIn.jsx
+++ src/components/signin/components/SignIn.jsx
@@ -1,13 +1,12 @@
-/* eslint-disable react/jsx-fragments */
 import React from 'react';
 import FormView from './FormView';
 import PageHelmet from './PageHelmet';
 
 export function SignIn() {
     return (
-        <>
+        <div>
             <PageHelmet />
             <FormView />
-        </>
+        </div>
     );
 }
