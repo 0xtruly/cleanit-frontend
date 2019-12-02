c6a3238c4e7c87093fa7c50b9865ff3aab5534ba
Edward Bella <eddydambel@gmail.com>
2019-12-02 19:28:19 +0100

    Firebase ProjectID fix

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
     measurementId: process.env.REACT_APP_FIREBASE_MSG_ID,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    projectId: 'cleanit-7147d',
     storageBucket: process.env.REACT_APP_FIREBASE_STR_BUCKET,
 };
 
